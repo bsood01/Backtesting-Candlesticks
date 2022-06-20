56f2c28f378b0d757028341c59c991f29aa22d24
Bhavesh Sood <78615767+bsood01@users.noreply.github.com>
2022-06-20 23:39:05 +0100

    Working with Nifty 50

--- candlesticks.py
+++ candlesticks.py
@@ -7,6 +7,7 @@ import time
 import os
 import glob
 
+
 # threading 
 #takes in the array of stocks and returns a Dataframe associated with each in a dictionary
 def yahoo_data(symbol, date_from, date_to):
@@ -15,8 +16,8 @@ def yahoo_data(symbol, date_from, date_to):
     result={}
     for s in symbol:
         df = pdr.DataReader(s, 'yahoo', start=date_from, end=date_to)
-        ##
-        df.fillna(method="ffill")
+        #Remove rows where stock data is missing 
+        df.dropna()
         result[s]=df
     return result
 
@@ -24,8 +25,8 @@ def csv_data(date_from, stock_list):
     path=os.getcwd()
     path = path+ "\\historical_data"
     all_files = glob.glob(os.path.join(path, "*.csv"))
-
-    col_list=["Date","Open","High","Low","Close","Volume"]
+    #file name
+    col_list=["Date","Open","High","Low","Close"]
     result={}
     for f in all_files:
         #get the symbol for the dictionary to store data
@@ -37,43 +38,43 @@ def csv_data(date_from, stock_list):
             df.set_index("Date",inplace = True)
             df.index = pd.to_datetime(df.index)  
             df=df[(df.index > date_from)]
-            #filling missing values using forward fill to avoid look-ahead bias
-            df.fillna(method="ffill")
+            #Remove rows where stock data is missing 
+            df.dropna()
 
             result[symbol]=df
 
 
     return result
 
-def backtest_stratergy(df):
+def backtest_stratergy(stocks):
     signals=[]
-    test_dates=df.index.tolist()
-    for date in test_dates:
-        tmp=candlestick_indicators(df,date)
-        #if we found a signal add it to our results dictinary
-        #No. of signals 
-        if tmp !=None:
-            signals.append(tmp)
-    #for d in signals:
-        #pretty(d)
+    for symbol in stocks:
+        test_dates=stocks[symbol].index.tolist()
+        for date in test_dates:
+            tmp=candlestick_indicators(stocks[symbol],date,symbol)
+            #if we found a signal add it to our results dictinary
+            if tmp !=None:
+                signals.append(tmp)
     return pd.DataFrame(signals)
 
      
 
-def candlestick_indicators(df,date):
+def candlestick_indicators(df,date, symbol):
 
-    # Volume???
     signal={}
-    trend=line_trend(df,date)
-    if dragonfly_doji(df, date) and trend==0:
+    trend=line_trend(df,date,20)
+    signal["Symbol"]=symbol
+    if trend==0 and dragonfly_doji(df, date):
         signal["Date"]=date
         signal["Indicator"]="Dragonfly Doji"
         signal["Reversal"]=True
-        signal["ConfirmationRequired"]=False
-        signal["NextCandle"]="NA"
+        signal["ConfirmationRequired"]=True
+        signal["NextCandle"]="HigherOpen"
         signal["EntryPrice"]=df.loc[date]["Close"]
         signal["TrendDirection"]="Down Trend"
         signal["StopLoss"]=df.loc[date]["Low"]
+        ##signal and variat strategy
+        ##2:1 ratio variant 
         #2:1 ratio for profit and stoploss
         signal["TakeProfit"]=signal["EntryPrice"]+2*(signal["EntryPrice"]-signal["StopLoss"])
         signal["Position"]="Long"
@@ -81,25 +82,25 @@ def candlestick_indicators(df,date):
 
         return signal
 
-    elif gravestone_doji(df,date) and trend==1:
+    elif trend==1 and gravestone_doji(df,date):
         signal["Date"]=date
         signal["Indicator"]="Gravstone Doji"
         signal["Reversal"]=True
-        signal["ConfirmationRequired"]=False
-        signal["NextCandle"]="NA"
+        signal["ConfirmationRequired"]=True
+        signal["NextCandle"]="LowerOpen"
         signal["EntryPrice"]=df.loc[date]["Close"]
         signal["TrendDirection"]="Up Trend"
         signal["StopLoss"]=df.loc[date]["High"]
         #2:1 ratio for profit and stoploss
         signal["TakeProfit"]=signal["EntryPrice"]-2*(signal["StopLoss"]-signal["EntryPrice"])
         signal["Position"]="Short"
+        signal["Variant"]=""
         test_short_indication(df,date,signal)
 
         return signal
 
-
-
     return None
+
 def test_long_indication(df, indication_date, signal_dict ):
     tmp_date= indication_date
     #3 months, 75 day cuttoff - conditional exit 
@@ -109,22 +110,22 @@ def test_long_indication(df, indication_date, signal_dict ):
         index=df.index.get_loc(tmp_date) + 1
         if index < df.index.size:
             tmp_date=df.index[index]
-            if df.loc[tmp_date,'Open']<signal_dict["StopLoss"]:
+            if df.loc[tmp_date,'Open']<=signal_dict["StopLoss"]:
                 signal_dict["ExitPrice"]=df.loc[tmp_date,'Open']
                 signal_dict["Win"]=False
                 signal_dict["ExitDate"]=tmp_date
                 return signal_dict
-            elif df.loc[tmp_date,'Low']<signal_dict["StopLoss"]:
+            elif df.loc[tmp_date,'Low']<=signal_dict["StopLoss"]:
                 signal_dict["ExitPrice"]=signal_dict["StopLoss"]
                 signal_dict["Win"]=False
                 signal_dict["ExitDate"]=tmp_date
                 return signal_dict
-            elif df.loc[tmp_date,'Open']>signal_dict["TakeProfit"]:
+            elif df.loc[tmp_date,'Open']>=signal_dict["TakeProfit"]:
                 signal_dict["ExitPrice"]=df.loc[tmp_date,'Open']
                 signal_dict["Win"]=True
                 signal_dict["ExitDate"]=tmp_date
                 return signal_dict
-            elif df.loc[tmp_date,'High']>signal_dict["TakeProfit"]:
+            elif df.loc[tmp_date,'High']>=signal_dict["TakeProfit"]:
                 signal_dict["ExitPrice"]=signal_dict["TakeProfit"]
                 signal_dict["Win"]=True
                 signal_dict["ExitDate"]=tmp_date
@@ -136,6 +137,7 @@ def test_long_indication(df, indication_date, signal_dict ):
     #exit after 75 days
     signal_dict["ExitPrice"]=df.loc[tmp_date,'Close']
     signal_dict["Win"]=signal_dict["ExitPrice"]>signal_dict["EntryPrice"]
+    signal_dict["ExitDate"]=tmp_date
     return signal_dict
 
 def test_short_indication(df, indication_date, signal_dict ):
@@ -202,15 +204,15 @@ def up_down_trend(df,date):
     else:
         return 2
 
-#returns the trend based on line of best fit of closing price if last 10 days
-def line_trend(df, date):
+#returns the trend based on line of best fit of closing price if last x days
+def line_trend(df, date, days):
 
     x=[]
     y=[]
-    cur_date=date
-    for i in range(5):
+    cur_date=df.index[df.index.get_loc(date)-1]
+    for i in range(days):
         if cur_date in df.index:
-            x.append(4-i)
+            x.append(days-1-i)
             y.append(df.loc[cur_date]["Close"])
             cur_date=df.index[df.index.get_loc(cur_date)-1]
 
@@ -259,7 +261,8 @@ def dragonfly_doji(df, date):
     #check for small top shadow
     df_doji= (hi-max(op,cl)<=total_length(df,date)*0.1)
 
-    if doji(df,date) and df_doji:
+    #and opperator
+    if df_doji and doji(df,date):
         return 1
 
     return 0
@@ -274,7 +277,7 @@ def gravestone_doji(df, date):
     #check for small top shadow
     gr_doji= (min(op,cl)-lo<=total_length(df,date)*0.1)
 
-    if doji(df,date) and gr_doji:
+    if   gr_doji and doji(df,date):
         return 1
 
     return 0
@@ -328,30 +331,24 @@ def pretty(d, indent=0):
          print('\t' * (indent+1) + str(value))    
 
 def main():
+    #nifty 50
+    #threads
+    df = pd.read_csv("nifty50list.csv",encoding= 'unicode_escape')
+    stock_list=df["Symbol"].values.tolist()
     '''df = pd.read_csv("allstocks.csv",encoding= 'unicode_escape')
-    stock_list=[]
     for index, row in df.iterrows():
-        if row['Active']==True:
-            stock_list.append(row['NSESymbol'])
-    #a Dict with Data frames of all the stocks
-    stocks=csv_data(date_from='2012-01-01', stock_list=stock_list)'''
+        if row['N']==True:
+            stock_list.append(row['NSESymbol'])'''
 
-    stocks=yahoo_data(['HINDUNILVR.NS'], date_from = '2012-01-01',date_to=dt.today().strftime('%Y-%m-%d'))
+    #a Dict with Data frames of all the stocks
+    stocks=csv_data(date_from='2012-01-01', stock_list=stock_list)
 
     path=os.getcwd()
     path = path+ "\\backtest_results"
     if not os.path.exists(path):
         os.makedirs(path)
     
-    for symbol in stocks:
-        #date_time_str = '22/06/12 00:00:00'
-        #date_time_obj = dt.strptime(date_time_str, '%d/%m/%y %H:%M:%S')
-        #line_trend(stocks[symbol],date_time_obj)
-        ema(stocks[symbol],10)
-        #stocks[symbol].iloc[2100:2300]['Close'].plot(label = 'TCS', figsize = (15,7))
-        #stocks[symbol].iloc[2100:2300]['EMA'].plot(label = "Infosys")
-        #plt.show()
-        backtest_stratergy(stocks[symbol]).to_csv(path+"\\"+symbol+".csv", encoding='utf-8', index=False)
+    backtest_stratergy(stocks).to_csv(path+"\\results.csv", encoding='utf-8', index=False)
 
     return 0
 
